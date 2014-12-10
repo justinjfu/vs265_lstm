5531d6ce6e2aa9a834f1877fdcf3f3b065b8561f
Justin Fu <justin.fu@reflektion.com>
2014-12-09 16:54:22 -0800

    Add placeholders for weights

--- src/lstm.py
+++ src/lstm.py
@@ -3,7 +3,7 @@ from objective import Objective, Weights
 
 
 class LSTMObjective(Objective):
-    def __init__(self):
+    def __init__(self, training_set):
         super(LSTMObjective, self).__init__()
 
     def gradient_at(self, wts):
@@ -16,6 +16,7 @@ class LSTMObjective(Objective):
 class LSTMWeights(Weights):
     def __init__(self):
         super(LSTMWeights, self).__init__()
+        self.layers = []
 
     def add_weight(self, other_weight):
         raise NotImplemented
@@ -27,5 +28,36 @@ class LSTMWeights(Weights):
         raise NotImplemented
 
     def __str__(self):
-        return str(self.val)
+        raise NotImplemented
+
+#Feedforward pass
+class LSTMLayerActivations(object):
+    def __init__(self, n):
+        pass
+
+    def forward(self, weights, prev_cell, input):
+        pass
+
+class LSTMLayerWeights(object):
+    def __init__(self, n, n_input, act_f, act_g, act_h):
+        self.act_f = act_f  # activation function on gates
+        self.act_g = act_g  # activation function on inputs
+        self.act_h = act_h  # activation function on ouputs
+
+        self.forgetw_x = np.zeros(n, n_input)  # forget weights from X
+        self.forgetw_i = np.zeros(n, n_input)  # forget weights from previous hidden
+        self.forgetw_c = np.zeros(n, n)  # forget weights from previous cell state
+
+        self.inw_x = np.zeros(n, n_input)  # input weights from X
+        self.inw_i = np.zeros(n, n_input)  # input weights from previous hidden
+        self.inw_c = np.zeros(n, n)  # input weights from previous cell state
+
+        self.outw_x = np.zeros(n, n_input)  # output weights from X
+        self.outw_i = np.zeros(n, n_input)  # output weights from previous hidden
+        self.outw_c = np.zeros(n, n)  # output weights from current cell state
+
+        self.cellw_x = np.zeros(n, n_input)  # cell state weights from X
+        self.cellw_h = np.zeros(n, n_input)  # cell state  weights from previous hidden
+
+
 
