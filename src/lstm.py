f5b64e4eeb8f4ea0e1310f82a0835fbe3345d31b
Siddhartho Bhattacharya <sid_gogol@hotmail.com>
2014-12-11 20:48:56 -0800

    added numerical gradient test for feed forward - not working yet

--- src/lstm.py
+++ src/lstm.py
@@ -1,5 +1,6 @@
 import numpy as np
 from objective import Objective, Weights
+from activations import Logistic
 
 
 class LSTMObjective(Objective):
@@ -11,9 +12,8 @@ class LSTMObjective(Objective):
     def gradient_at(self, wts):
         raise NotImplemented
 
-    def value_at(self, wts):
-        raise 
-
+    def value_at(self, wts, label):
+        return 0.5*(wts - label)**2
 
 class LSTMWeights(Weights):
     def __init__(self):
@@ -32,9 +32,8 @@ class LSTMWeights(Weights):
     def __str__(self):
         raise NotImplemented
 
-
 class LSTMLayerWeights(object):
-    def __init__(self, n, n_input, act_f, act_g, act_h):
+    def __init__(self, n, n_input, n_output, act_f, act_g, act_h):
         self.n = n  # number of units on this layer
         self.n_input = n_input  # number of inputs into this layer
 
@@ -57,7 +56,7 @@ class LSTMLayerWeights(object):
         self.cellw_x = np.random.uniform(-1, 1, (n, n_input))  # cell state weights from X
         self.cellw_h = np.random.uniform(-1, 1, (n, n))  # cell state weights from previous hidden
 
-        self.final_output_weights = np.random.uniform(-1, 1, (n,n)) # layer output weights
+        self.final_output_weights = np.random.uniform(-1, 1, (n_output, n)) # layer output weights
 
     def forward(self, previous_cell, previous_hidden, previous_layer_input):
         """
@@ -91,7 +90,7 @@ class LSTMLayerWeights(object):
 
         return new_cell_states, new_hidden, output
 
-    def backward(self, next_cell, next_hidden, next_layer_output):
+    def backward(self, nxt_cell, next_hidden, next_layer_output):
         """
         Compute backward activations
         :param previous_cell:
@@ -102,4 +101,72 @@ class LSTMLayerWeights(object):
         pass
 
 if __name__ == '__main__':
-    pass
+    # test on bitstring parity checker - tests feed forward only with numerical gradient calculation
+    f, g, h = Logistic(), Logistic(), Logistic() 
+    lstm = LSTMLayerWeights(2, 1, 1, f, g, h)
+    
+    N=10
+    trainingIn = np.array([1,0,0,0,0,0,0,1]*N)
+    trainingOut = np.cumsum(trainingIn)
+   
+    weights = np.array([lstm.forgetw_x,
+                        lstm.forgetw_h,
+                        lstm.forgetw_c,
+                                     
+                        lstm.inw_x,
+                        lstm.inw_h,
+                        lstm.inw_c,
+                                   
+                        lstm.outw_x,
+                        lstm.outw_h,
+                        lstm.outw_c,
+                                                   
+                        lstm.cellw_x,
+                        lstm.cellw_h,
+                        lstm.final_output_weights])
+
+    d_weights = np.array([np.zeros(lstm.forgetw_x.shape),
+                          np.zeros(lstm.forgetw_h.shape),
+                          np.zeros(lstm.forgetw_c.shape),
+                                                
+                          np.zeros(lstm.inw_x.shape),
+                          np.zeros(lstm.inw_h.shape),
+                          np.zeros(lstm.inw_c.shape),
+                                                
+                          np.zeros(lstm.outw_x.shape),
+                          np.zeros(lstm.outw_h.shape),
+                          np.zeros(lstm.outw_c.shape),
+                                                              
+                          np.zeros(lstm.cellw_x.shape),
+                          np.zeros(lstm.cellw_h.shape),
+                          np.zeros(lstm.final_output_weights.shape)])
+
+
+
+    perturb_amount = 1e-5
+    ObjF = LSTMObjective(trainingIn) 
+    cs, hs = np.zeros((2,1)), np.zeros((2,1))
+    for t in range(len(trainingIn)):
+        for wi in range(len(weights)): # need to iterate through each weight independently
+            weight = weights[wi]
+            dweight = d_weights[wi] # dweight is where we store the gradients for each particular weight
+            for index, val in np.ndenumerate(weight): # a 'single' weight is a matrix, so we iterate within as well
+
+                weight[index] -= perturb_amount # w_ij - e
+                _, _, temp_out_old = lstm.forward(cs, hs, trainingIn[t]) # the output for this training sample 
+                lessObj = ObjF.value_at(temp_out_old, trainingOut[t]) # the error with negative perturb
+
+                weight[index] += 2*perturb_amount # w_ij + e
+                _, _, temp_out_new = lstm.forward(cs, hs, trainingIn[t]) # output for this training sample
+                moreObj = ObjF.value_at(temp_out_new, trainingOut[t]) # the error with positive perturb
+                diff = moreObj - lessObj # difference
+                #import pdb; pdb.set_trace()
+                grad = -1*diff/(2*perturb_amount) # negative gradient
+                weight[index] -= perturb_amount # set weight back to normal
+                dweight[index] = grad # save gradient, will update all the weights at the end
+        weights += d_weights # update all the weights for this training sample!
+        cs, hs, o = lstm.forward(cs, hs, trainingIn[t]) # calc the real error
+        #import pdb; pdb.set_trace()
+        print o, ObjF.value_at(o, trainingOut[t])
+            
+    
